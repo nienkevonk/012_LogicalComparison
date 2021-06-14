3db76d0fa95c16078eadcfac4fb74de86ee8561c
nienkevonk <84348574+nienkevonk@users.noreply.github.com>
2021-06-14 20:50:28 +0200

    Add files via upload
    Na nakijken

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ if (age >= 18 && age <= 25) {
 } else if (age > 25) {
     console.log("Je mag naar binnen!");
 }
+const driverStatus = "bob";
+if (driverStatus === "bob") {
+    console.log("Je bent de bob, je mag autorijden");
+} else {
+    console.log("Je bent geen bob, hier met die autosleutels");
+}
+
 const name = "Sarah";
 if (name == "Bram" || name == "Sarah") {
     console.log("Je hebt recht op een gratis biertje!");
@@ -13,16 +20,13 @@ if (name == "Bram" || name == "Sarah") {
     console.log("Je heet helaas geen Sarah of Bram.");
 }
 
-const totalAmount = 51;
-if (totalAmount <= 25) {
-    console.log("Geef meer geld uit om iets extra's te krijgen");
-}
-else if (totalAmount >= 50 && totalAmount < 100) {
+const totalAmount = 23;
+if (totalAmount >= 100) {
+    console.log("Je krijgt een gratis flesje champagne.");
+} else if (totalAmount > 50) {
     console.log("Je krijgt een gratis portie nachos");
-}
-else if (totalAmount > 25 && totalAmount <= 50) {
-    console.log("Je krijgt een gratis portie (vega)bitterballen!");
-}
-else if (totalAmount >= 100) {
-    console.log("Je krijgt een gratis fles champagne");
+} else if (totalAmount > 25) {
+    console.log("Je krijgt gratis (vega)bitterballen");
+} else {
+    console.log("Helaas niks extra's, maar leuk dat je er bent. Geef meer geld uit.");
 }
